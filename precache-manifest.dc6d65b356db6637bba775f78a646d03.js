54649f9ded2351fb4921e0349c93b82b2feeed6c
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-03-27 13:06:51 -0500

    Updates

---
+++ precache-manifest.dc6d65b356db6637bba775f78a646d03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32f11afa35bf9986bf8973aa497dc7d7",
+    "url": "/daniel-schroeder-dev/hacker-news-top-stories/index.html"
+  },
+  {
+    "revision": "a60ddc89a80cf4dfbc03",
+    "url": "/daniel-schroeder-dev/hacker-news-top-stories/static/css/main.1e29f0d7.chunk.css"
+  },
+  {
+    "revision": "c2b1a42c4e965435dba0",
+    "url": "/daniel-schroeder-dev/hacker-news-top-stories/static/js/2.7cbd0989.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/daniel-schroeder-dev/hacker-news-top-stories/static/js/2.7cbd0989.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a60ddc89a80cf4dfbc03",
+    "url": "/daniel-schroeder-dev/hacker-news-top-stories/static/js/main.ca2ab589.chunk.js"
+  },
+  {
+    "revision": "60635520f7f51ef4a63e",
+    "url": "/daniel-schroeder-dev/hacker-news-top-stories/static/js/runtime-main.8c1e2097.js"
+  }
+]);
