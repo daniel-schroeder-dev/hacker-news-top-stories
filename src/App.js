bc72bde2accf18de2c0a524872447a1ebc5f9cde
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-03-26 23:06:58 -0500

    Pull data from HN servers and render.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from './components/Card/Card';
 
 import './App.css';
 
 function App() {
+
+  const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+
+    const url = 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty';
+    const itemBaseUrl = 'https://hacker-news.firebaseio.com/v0/item/';
+
+    const topStoriesResponse = await fetch(url);
+    let topStoriesData = await topStoriesResponse.json();
+    topStoriesData = topStoriesData.slice(0, 20).map(storyID => fetch(`${itemBaseUrl}${storyID}.json?print=pretty`));
+
+    const responses = await Promise.all(topStoriesData)
+    const data = await Promise.all(responses.map(response => response.json()));
+
+    return data;
+  };
+
+  const chill = foo => setTimeout(() => { foo(false) }, 3000);
+
+  useEffect(() => {
+    fetchData()
+      .then(articles => {
+        setTimeout(() => {
+          setArticles(articles);
+          setIsLoading(false);
+        }, 1000);
+      });
+  }, []);
+
   return (
     <main className="app">
-      <Card />
-      <Card /> 
-      <Card />       
+    {isLoading && "Loading..."}
+    {articles.length ? articles.map((article, i) => (<Card key={article.id} by={article.by} title={article.title} number={i + 1} url={article.url} />)) : null}     
     </main>
   );
 }
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import './Card.css';
 
 function Card(props) {
   return (
     <li className="card">
-      <div className="card__item-number"><span className="card__item-number__number">1</span></div>
-      <a className="card__article-title-link" href="#">Article title</a> <span className="card__article-title-by">by</span> <span className="card__article-title-author">author</span>
+      <div className="card__item-number"><span className="card__item-number__number">{props.number}</span></div>
+      <a className="card__article-title-link" href={props.url}>{props.title}</a> <span className="card__article-title-by">by</span> <span className="card__article-title-author">{props.by}</span>
     </li>
   );
 }
+
+Card.propTypes = {
+  number: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  by: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+};
 
 export default Card;
